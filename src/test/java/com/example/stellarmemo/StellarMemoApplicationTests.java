08de7663cfbc525924b80b1cdf306f64d9a7b7f2
ikka <ikka0426@gmail.com>
2023-06-19 11:56:08 +0800

    创建项目

---
+++ src/test/java/com/example/stellarmemo/StellarMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stellarmemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StellarMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
