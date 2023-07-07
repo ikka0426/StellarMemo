9293275b15851e290ecd1c1d4d5440bed5ac6aaa
chafferexb <92236807+chafferexb@users.noreply.github.com>
2023-07-07 11:31:35 +0800

    完成根据关键字搜索和根据tag搜索

--- src/main/java/com/example/stellarmemo/dao/NoteDao.java
+++ src/main/java/com/example/stellarmemo/dao/NoteDao.java
@@ -11,4 +11,8 @@ public interface NoteDao {
     public void createNote(@Param("user_id")String user_id,@Param("content") String content,@Param("noteid") String noteid);//创建笔记
     public void modifyNote(@Param("noteid") String noteid,@Param("content") String content);//修改笔记
     public void deleteNote(@Param("noteid") String noteid);//删除笔记
+    public List<Note> searchByKey(@Param("key") String key,@Param("startIndex") int startIndex,@Param("pageSize") int pageSize);
+    public List<Note> searchByTag1(@Param("tag1") String tag1);
+    public List<Note> searchByTag2(@Param("tag1") String tag1,@Param("tag2") String tag2);
+    public List<Note> searchByTag3(@Param("tag1") String tag1,@Param("tag2") String tag2,@Param("tag3") String tag3);
 }
