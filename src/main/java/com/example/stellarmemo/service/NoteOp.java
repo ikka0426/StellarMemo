e324fbdf6c967aa50fd6ee66f287e3c7a5b8f6f5
chafferexb <92236807+chafferexb@users.noreply.github.com>
2023-07-07 11:32:13 +0800

    完成根据关键字搜索和根据tag搜索

--- src/main/java/com/example/stellarmemo/service/NoteOp.java
+++ src/main/java/com/example/stellarmemo/service/NoteOp.java
@@ -7,4 +7,6 @@ public interface NoteOp {
     public WebResult createNote(String user_id,String content,String noteid);
     public WebResult modifyNote(String noteid,String content);
     public WebResult deleteNote(String noteid);
+    public List<Note> searchByKey(String key,int startIndex,int pageSize);
+    public List<Note> searchByTag(String tag1,String tag2,String tag3);
 }
